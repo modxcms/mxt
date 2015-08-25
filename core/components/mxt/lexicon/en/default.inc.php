406a1cc6547c2dd377f8cdc6cf9e7a310829f001
Wayne Roddy <wayneroddy@Waynes-MacBook-Pro.local>
2015-08-25 17:52:55 -0400

    Lexicon updates

--- core/components/mxt/lexicon/en/default.inc.php
+++ core/components/mxt/lexicon/en/default.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 $_lang['setting_mxt.theme'] = 'Theme Name';
-$_lang['setting_mxt.theme_desc'] = 'Name od the theme to use';
+$_lang['setting_mxt.theme_desc'] = 'Name of the theme to use';
 $_lang['setting_mxt.default_slogan'] = 'Default Theme Slogan';
 $_lang['setting_mxt.default_slogan_desc'] = '';
 $_lang['setting_mxt.default_copyright'] = 'Default Theme Copyright';
@@ -12,6 +12,8 @@ $_lang['setting_mxt.default_fposts'] = 'Default Featured Post IDs';
 $_lang['setting_mxt.default_fposts_desc'] = '';
 $_lang['setting_mxt.default_carousel'] = 'Default Carousel IDs';
 $_lang['setting_mxt.default_carousel_desc'] = '';
+$_lang['setting_mxt.default_slider'] = 'Default Slider IDs';
+$_lang['setting_mxt.default_slider_desc'] = '';
 $_lang['setting_mxt.social_twitter'] = 'Twitter';
 $_lang['setting_mxt.social_twitter_desc'] = '';
 $_lang['setting_mxt.social_facebook'] = 'Facebook';
